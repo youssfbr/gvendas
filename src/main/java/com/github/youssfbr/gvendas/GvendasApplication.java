4455299b0409faf9b261d45b589ebc00268ef536
Alisson Youssf <youssfbr@gmail.com>
2023-11-23 00:00:51 -0300

    Project created

---
+++ src/main/java/com/github/youssfbr/gvendas/GvendasApplication.java
@@ -0,0 +1,13 @@
+package com.github.youssfbr.gvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GvendasApplication.class, args);
+	}
+
+}
