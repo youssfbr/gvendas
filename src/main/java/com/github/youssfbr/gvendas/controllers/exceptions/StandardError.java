4978f68a5849a9b5a229bdb8fc9137755d4084ef
Alisson Youssf <youssfbr@gmail.com>
2023-11-25 03:52:55 -0300

    Refactoring Category Validation

--- src/main/java/com/github/youssfbr/gvendas/controllers/exceptions/StandardError.java
+++ src/main/java/com/github/youssfbr/gvendas/controllers/exceptions/StandardError.java
@@ -1,22 +1,17 @@
 package com.github.youssfbr.gvendas.controllers.exceptions;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.Getter;
 
 import java.time.Instant;
-import java.util.ArrayList;
 import java.util.List;
 
-@Data
+@Getter
 @Builder
-@AllArgsConstructor
-@NoArgsConstructor
 public class StandardError {
     private Instant timestamp;
     private Integer status;
     private String error;
     private String path;
-    private List<Error> errors = new ArrayList<>();
+    private List<Error> errors;
 }
